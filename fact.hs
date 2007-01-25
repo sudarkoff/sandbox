8c16879a81dcb8d5531879c71a5e34fdcbaac2df
George Sudarkoff <george@sudarkoff.com>
2007-01-25 08:26:39 +0000

    fixed output in fact.hs

--- fact.hs
+++ fact.hs
@@ -1,7 +1,8 @@
-module Fact where
+module Main where
 
-fact :: Integer -> Integer
+-- fact :: Integer -> Integer
 fact 0 = 1
 fact n = n * fact (n - 1)
 
-main = fact 42
+f_print n = print(show n ++ "th factorial is " ++ show (fact n))
+main = f_print 15
