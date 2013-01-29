24e6a669a936c7f6f3c91d3be8bafdb918fb15df
George Sudarkoff <george@virsto.com>
2013-01-29 10:40:27 -0800

    Fix indentation.

--- towersOfFacebook.hs
+++ towersOfFacebook.hs
@@ -5,13 +5,13 @@ hanoiR :: Integer -> [a] -> [(a, a)]
 hanoiR 0 _ = []
 
 -- one disk: one move and two pegs needed.
-hanoiR 1 (p1 : p2 : rest) = [(p1, p2)] -- only needed for smart-alecks?
+hanoiR 1 (p1 : p2 : rest) = [(p1, p2)]
 
 -- n disks and r > 3 pegs: use Frame-Stewart algorithm
 hanoiR n (p1 : p2 : p3 : rest) =
-    hanoiR k (p1 : p3 : p2 : rest) ++
-    hanoiR (n - k) (p1 : p2 : rest) ++
-    hanoiR k (p3 : p2 : p1 : rest)
-    where k
-        | (null rest)   = n - 1
-        | otherwise     = n `div` 2
+   hanoiR k (p1 : p3 : p2 : rest) ++
+   hanoiR (n - k) (p1 : p2 : rest) ++
+   hanoiR k (p3 : p2 : p1 : rest)
+   where k
+            | (null rest)   = n - 1
+            | otherwise     = n `div` 2
